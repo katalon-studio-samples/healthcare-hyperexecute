ec5fed06dae9573b064f548e59c10371ab379443
Coty Rosenblath <coty@katalon.com>
2024-10-16 13:11:53 -0400

    Initial commit

---
+++ Test Suites/Dynamic Test Suite.ts
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<FilteringTestSuiteEntity>
+   <description></description>
+   <name>Dynamic Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>8eb6d2a6-1a15-4b75-a075-d20a4c1ea735</testSuiteGuid>
+   <filteringBuiltIn>com.kms.katalon.execution.platform.DynamicBuiltInSearch</filteringBuiltIn>
+   <filteringExtension></filteringExtension>
+   <filteringPlugin></filteringPlugin>
+   <filteringText>name=(TC1)</filteringText>
+</FilteringTestSuiteEntity>
